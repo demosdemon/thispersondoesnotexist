bfe85df831ac2621d91fbc0318b396f71dd8ec89
Brandon LeBlanc <brandon@leblanc.codes>
2019-02-14 22:00:03 -0600

    stub project using https://github.com/sloria/cookiecutter-flask

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn thispersondoesnotexist.app:create_app\(\) -b 0.0.0.0:$PORT -w 3
---
+++ autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from thispersondoesnotexist.app import create_app
+
+app = create_app()
